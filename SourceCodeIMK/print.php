1afaebe43c0f9f1dfc9428c3870f3bfa263cf010
Nehemiah Austen Pison <58833986+EzraelVio@users.noreply.github.com>
2020-12-12 19:16:44 +0700

    Add files via upload
    sistem print tabel matkul
    
    udah bisa:
    print senin-minggu
    print jam ke 1-12
    
    belom bisa:
    print hari dinamis (misal senin - jumat doang)
    print jam dinamis (misal jam 1-16 bukan 1-12)

---
+++ SourceCodeIMK/print.php
@@ -0,0 +1,46 @@
+<?php
+$hari = array("Senin", "selasa", "Rabu", "Kamis", "Jumat", "Sabut", "Minggu");
+?>
+<table>
+    <thead>
+      <th>&nbsp;</th>
+    
+    <?php
+    for ($i = 1 ; $i < 13 ; $i++) {
+        echo "<th>" . $i . "</th>";
+    }
+    ?>
+    </thead>
+    <tbody>
+      <?php
+        for ($u = 0 ; $u < sizeof($hari) ; $u++) {
+
+            echo "<tr>";
+            echo "<td>" . $hari[$u] . "</td>";
+
+            for ($p = 1 ; $p < 13 ; $p++) {
+                $sql = "SELECT nama_matkul, keterangan, waktumulai, waktuakhir FROM matkul WHERE id_user = '$id_user' AND hari = '$hari[$u]' AND waktumulai = '$p'";
+                $statement = $conn->query($sql);
+                $matkulnya = $statement->fetch_all(MYSQLI_ASSOC);
+            
+            if (empty($matkulnya)) {
+                echo "<td>&nbsp;</td>";
+            } else {
+                $lama = $matkulnya[0]['waktuakhir'] - $matkulnya[0]['waktumulai'] +1;
+
+                echo "<td class=\"matkul\" colspan=\" " . $lama . "\">";
+                echo $matkulnya[0]['nama_matkul'] . "<br>" . $matkulnya[0]['keterangan'];
+                echo "</td>";
+
+                $p += $lama - 1;
+            }
+
+
+            }
+            echo "</tr>";
+        }
+
+      ?>
+    </tbody>
+  </table>
+</body>
