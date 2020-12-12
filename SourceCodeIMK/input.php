cd65059de45913e6760c224d264a4778dab7da1e
Nehemiah Austen Pison <58833986+EzraelVio@users.noreply.github.com>
2020-12-12 17:58:36 +0700

    Update input.php

--- SourceCodeIMK/input.php
+++ SourceCodeIMK/input.php
@@ -1,4 +1,7 @@
 <?php
+
+require 'config.php';
+
 $nama_matkul= $_POST['nama_matkul'];
 $keterangan = $_POST['keterangan'];
 $hari       = $_POST['hari'];
@@ -6,11 +9,11 @@ $waktumulai = $_POST['waktumulai'];
 $waktuakhir = $_POST['waktuakhir'];
 $id_user    = $_SESSION['id_user'];
 
-if (empty($nama_matkul||$keterangan||$hari||$waktumulai||$waktuakhir)) {
+if (empty($nama_matkul)||empty($keterangan)||empty($hari)||empty($waktumulai)||empty($waktuakhir)) {
     $message = "Semua field harus diisi!";
     echo "<script type='text/javascript'>alert('$message');</script>";
 } else {
-    $sql = "INSERT INTO matkul VALUES ('$nama_matkul', '$keterangan', '$hari', '$waktumulai', '$waktuakhir')";
+    $sql = "INSERT INTO matkul VALUES ('', '$nama_matkul', '$keterangan', '$hari', '$waktumulai', '$waktuakhir', '$id_user')";
     $statement = $conn->query($sql);
     $message = "Data berhasil diinput!";
     echo "<script type='text/javascript'>alert('$message');</script>";
