2163d0a69df75e63d068fc55e13e495a87f03af6
Nehemiah Austen Pison <58833986+EzraelVio@users.noreply.github.com>
2020-12-20 18:09:39 +0700

    Update user_set.php

--- SourceCodeIMK/user_set.php
+++ SourceCodeIMK/user_set.php
@@ -1,8 +1,8 @@
 <?php
+    date_default_timezone_set('Asia/Jakarta');
+    
     if (isset($_SESSION['id_user'])) {
     $id_user = $_SESSION['id_user'];
-    $message = "Anda masuk kedalam sesi sebelumnya";
-    echo "<script type='text/javascript'>alert('$message');</script>";
     } else {
     $id_user = rand(1000000000,9999999999);
     
@@ -11,7 +11,7 @@
     $data_pengguna = $statement->fetch_all(MYSQLI_ASSOC);
 
     if (empty($data_pengguna)) {
-        $waktu_dibuat = date("d.m.y");
+        $waktu_dibuat = date("d.m.y H:i:s");
         $sql = "INSERT INTO pengguna VALUES ('$id_user', '$waktu_dibuat')";
         $statement = $conn->query($sql);
     }
