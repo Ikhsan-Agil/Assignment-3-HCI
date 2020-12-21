2f5b9f55ba3bc555f0dceaad2ae69feaf663da25
Ikhsan Agil Kusuma <72031271+Ikhsan-Agil@users.noreply.github.com>
2020-12-21 20:28:10 +0700

    Update index.php
    penyesuaian dengan index.css

--- SourceCodeIMK/index.php
+++ SourceCodeIMK/index.php
@@ -1,65 +1,81 @@
 <?php
-    session_start();
-    require 'config.php';
-    require 'user_set.php';
+session_start();
+require 'config.php';
+require 'user_set.php';
 ?>
 
 <!DOCTYPE HTML>
 <html>
 
 <head>
-  <title>Jadwaldotcom</title>
-  <?php
-  require 'script.php';
-  ?>
+    <title>Jadwaldotcom</title>
+    <?php
+    //require 'script.php';
+    ?>
+    
+    <link rel="stylesheet" href="index.css">
+    
+    <!-- Google Fonts -->
+    <!-- Roboto -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"> 
 </head>
 
 <body>
-    <h1>JADWAL .(DOT) COM</H1>
-    <form id="formtabel" method="POST">
-    <input type="text" name="nama_matkul" maxlength="20" required/>
-    <input type="text" name="keterangan" maxlength="20" required/>
-    <select name="hari" required>
-        <option>Senin</option>
-        <option>Selasa</option>
-        <option>Rabu</option>
-        <option>Kamis</option>
-        <option>Jumat</option>
-        <option>Sabtu</option>
-        <option>Minggu</option>
-    </select>
-    <select name="waktumulai" required>
-    <?php
-        for ($i = 1; $i < 12; $i++) {
-            echo "<option>" . $i . "</option>";
-        }
-    ?>
-    </select>
-    <select name="waktuakhir" required>
-    <?php
-        for ($i = 1; $i < 12; $i++) {
-            echo "<option>" . $i . "</option>";
-        }
-    ?>
-    </select>
-    <input type='submit' id='submit' value='submit'>
-    </form>
-    
-    <div id="status"></div>
-    <form id="caridata" method="POST" action="cariid.php">
-    <input type="number" name="loadid" maxlength="10" required>
-    <input type='submit' id='submit' value='cari data'>
-    </form>
+    <header>
+        <h1>JADWAL .(DOT) COM</h1>
+        <form id="caridata" method="POST" action="cariid.php">
+            <input type="number" name="loadid" id="search" maxlength="10" required>
+            <button type='submit' id='btncari'>
+                Cari Data
+            </button>
+        </form>
+    </header>
 
-    <form id="print" method="POST" action="#">
-    <input type="hidden" name="user_id" maxlength="10" value="<?php echo $id_user; ?>">
-    <input type='submit' id='submit' value='print'>
-    </form>
+    <main>
+        <form id="formtabel" method="POST">
+            <input type="text" name="nama_matkul" id="matkul" maxlength="20" placeholder="Nama Matkul" required />
+            <input type="text" name="keterangan" id="keterangan" maxlength="20" placeholder="Keterangan" required />
+            <select name="hari" id="hari" required>
+                <option>Senin</option>
+                <option>Selasa</option>
+                <option>Rabu</option>
+                <option>Kamis</option>
+                <option>Jumat</option>
+                <option>Sabtu</option>
+                <option>Minggu</option>
+            </select>
+            <select name="waktumulai" id="mulai" required>
+                <?php
+                for ($i = 1; $i < 12; $i++) {
+                    echo "<option>" . $i . "</option>";
+                }
+                ?>
+            </select>
+            <select name="waktuakhir" id="selesai" required>
+                <?php
+                for ($i = 1; $i < 12; $i++) {
+                    echo "<option>" . $i . "</option>";
+                }
+                ?>
+            </select>
+            <button type='submit' name="submit" id='submit'>
+                Submit
+            </button>
+            <input type="hidden" name="user_id" maxlength="10" value="<?php echo $id_user; ?>">
+            <button type='submit' name="print" id='btnprint'>
+                Cetak
+            </button>
+        </form>
 
-    <div id="printer">
-    <?php
-    require 'print.php';
-    ?>
-    </div>
+        <div id="status"></div>
+
+        <div id="printer">
+            <?php
+            require 'print.php';
+            ?>
+        </div>
+    </main>
 </body>
+
 </html>
